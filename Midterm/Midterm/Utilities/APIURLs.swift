6f9c96b2d663059fde849bd892b1a140bdab25b5
jindinggan <56701855+jindinggan@users.noreply.github.com>
2022-04-10 16:19:17 -0400

    Add files via upload

---
+++ Midterm/Midterm/Utilities/APIURLs.swift
@@ -0,0 +1,10 @@
+//
+//  APIURLs.swift
+//  Midterm
+//
+//  Created by 靳丁干 on 4/10/22.
+//
+
+import Foundation
+
+let quotingURL = "https://financialmodelingprep.com/api/v3/quote/"
---
+++ Midterm/Midterm/Utilities/ProtocolSendStockData.swift
@@ -0,0 +1,12 @@
+//
+//  Protocol.swift
+//  Midterm
+//
+//  Created by 靳丁干 on 4/10/22.
+//
+
+import Foundation
+
+protocol SendStockDelegate{
+    func sendStockData(StockQuote: StockQuote)
+}
